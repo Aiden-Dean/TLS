bd614e182104731766fdb4561ca49608613fb448
aiden <aidenwelsh@outlook.com>
2020-11-24 14:51:29 +0000

    added server applet to test servers which have no endpoints to hit + refactors

---
+++ TLS.Client/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using Newtonsoft.Json;
+
+namespace TLS.Client
+{
+    [Command(Description = "A simple app to test SSL/TLS protocols for a specified endpoint.")]
+    [HelpOption("-?")]
+    internal class Program
+    {
+        [Option("-t|--target", Description = "The target endpoint to query. Defaults to 127.0.0.1")]
+        private string TargetEndpoint { get; } = IPAddress.Loopback.MapToIPv4().ToString();
+
+        [Option("-p|--port", Description = "The port to connect on. Defaults to 443.")]
+        private int Port { get; } = 443;
+
+        [Option("-v|--verbose")] private bool Verbose { get; } = false;
+
+        private static async Task<int> Main(string[] args) =>
+            await CommandLineApplication.ExecuteAsync<Program>(args);
+
+        private async Task<int> OnExecuteAsync(CommandLineApplication app,
+            CancellationToken cancellationToken = default)
+        {
+            if (string.IsNullOrWhiteSpace(TargetEndpoint))
+            {
+                app.ShowHelp();
+                return 0;
+            }
+
+            if (Verbose)
+            {
+                Console.WriteLine("Starting TLS.Client app...\n");
+            }
+
+            var status = new ProtocolStatus
+            {
+                TargetEndpoint = TargetEndpoint,
+                Port = Port
+            };
+
+            if (Verbose)
+                Console.WriteLine("Testing against list of known protocols.\n");
+
+            foreach (SslProtocols protocol in Enum.GetValues(typeof(SslProtocols)))
+            {
+                if (Verbose)
+                {
+                    Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                    Console.WriteLine($"Begin building test for protocol [{protocol}]");
+                }
+
+                var client = new TcpClient();
+
+                try
+                {
+                    if (Verbose)
+                        Console.WriteLine($"Attempting to connect to [{TargetEndpoint}:{Port}]");
+
+                    await client.ConnectAsync(TargetEndpoint, Port);
+                    status.ConnectionSuccessful = true;
+                    if (Verbose)
+                        Console.WriteLine(
+                            $"Connection [{(status.ConnectionSuccessful ? "Successful" : "Unsuccessful")}]");
+                }
+                catch
+                {
+                    status.ConnectionSuccessful = false;
+
+                    if (Verbose)
+                        Console.WriteLine(
+                            $"Connection [{(status.ConnectionSuccessful ? "Successful" : "Unsuccessful")}]");
+
+                    return 0;
+                }
+
+                if (Verbose)
+                    Console.WriteLine("Creating SSL/TLS connection stream");
+
+                var sslStream = new SslStream(client.GetStream(), false);
+
+                try
+                {
+                    if (Verbose)
+                        Console.WriteLine($"Attempting to authenticate as a client via protocol [{protocol}]");
+
+                    await sslStream.AuthenticateAsClientAsync(new SslClientAuthenticationOptions
+                    {
+                        TargetHost = TargetEndpoint,
+                        EnabledSslProtocols = protocol,
+                        CertificateRevocationCheckMode = X509RevocationMode.NoCheck,
+                        RemoteCertificateValidationCallback = RemoteCertificateValidationCallback
+                    }, cancellationToken);
+
+                    //sslStream.AuthenticateAsClient(Endpoint, null, protocol, false);
+
+                    if (Verbose)
+                    {
+                        Console.WriteLine(
+                            $"Authentication [{(sslStream.IsAuthenticated ? "Successful" : "Unsuccessful")}]");
+                        Console.WriteLine("Gathering certificate details");
+                    }
+
+                    status.Certificate = new Certificate
+                    {
+                        Subject = sslStream.RemoteCertificate?.Subject,
+                        Issuer = sslStream.RemoteCertificate?.Issuer,
+                        NotBefore = sslStream.RemoteCertificate?.GetEffectiveDateString(),
+                        NotAfter = sslStream.RemoteCertificate?.GetExpirationDateString(),
+                    };
+
+                    if (Verbose)
+                        Console.WriteLine($"Adding successful attempt for protocol [{protocol}]");
+
+                    status.Protocols.Add(protocol, true);
+                }
+                catch (AuthenticationException e)
+                {
+                    if (Verbose)
+                    {
+                        Console.WriteLine("Exception: {0}", e.Message);
+                        if (e.InnerException != null)
+                        {
+                            Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                        }
+                        Console.WriteLine ("Authentication failed - closing the connection.");
+                        Console.WriteLine($"Adding unsuccessful attempt for protocol [{protocol}]");
+                    }
+
+                    status.Protocols.Add(protocol, false);
+                }
+                finally
+                {
+                    if (Verbose)
+                        Console.WriteLine("Closing the SSL/TLS connection stream");
+
+                    sslStream.Close();
+
+                    if (Verbose)
+                        Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+                }
+            }
+
+            Console.WriteLine(JsonConvert.SerializeObject(status, Formatting.Indented));
+
+            return 1;
+        }
+
+        private static bool RemoteCertificateValidationCallback(object sender, X509Certificate certificate,
+            X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            // we do not care if the cert is invalid right now
+            return true;
+        }
+    }
+
+    internal class ProtocolStatus
+    {
+        public string TargetEndpoint { get; set; }
+        public int Port { get; set; }
+        public bool ConnectionSuccessful { get; set; }
+        public Dictionary<SslProtocols, bool> Protocols { get; } = new Dictionary<SslProtocols, bool>();
+        public Certificate Certificate { get; set; }
+    }
+
+    internal class Certificate
+    {
+        public string Subject { get; set; }
+        public string Issuer { get; set; }
+        public string NotBefore { get; set; }
+        public string NotAfter { get; set; }
+    }
+}
---
+++ TLS.Server/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+
+namespace TLS.Server
+{
+    [Command(Description = "A server applet that listens for incoming socket connections to test TLS.")]
+    [HelpOption("-?")]
+    // ReSharper disable once ClassNeverInstantiated.Global
+    internal class Program
+    {
+        [Option("-cf|--certFile", Description = "The machine certificate to be used to create a secure channel. " +
+                                           "Defaults to example cert included in the build.")]
+        private string CertificateFile { get; } = Path.Combine(Environment.CurrentDirectory, "example.pfx");
+
+        [Option("-cp|--certPass", Description = "The password to open an encrypted machine certificate.")] 
+        private string CertificatePassword { get; } = "test1234";
+
+        private static X509Certificate _certificate;
+
+        [Option("-p|--port", Description = "The port to communicate via. Defaults to 443.")]
+        private int Port { get; } = 443;
+
+        private static async Task<int> Main(string[] args) =>
+            await CommandLineApplication.ExecuteAsync<Program>(args);
+
+        private async Task<int> OnExecuteAsync(CommandLineApplication app,
+            CancellationToken cancellationToken = default)
+        {
+            if (string.IsNullOrWhiteSpace(CertificateFile))
+            {
+                app.ShowHelp();
+                return 0;
+            }
+
+            _certificate = new X509Certificate(CertificateFile, CertificatePassword);
+
+            var listener = new TcpListener(IPAddress.Any, Port);
+            listener.Start();
+
+            while (true)
+            {
+                Console.WriteLine("Waiting for a client to connect...");
+                var client = await listener.AcceptTcpClientAsync();
+                await ProcessClientConnectionAsync(client, cancellationToken);
+            }
+        }
+
+        private static async Task ProcessClientConnectionAsync(TcpClient client, CancellationToken cancellationToken = default)
+        {
+            var sslStream = new SslStream(client.GetStream(), false);
+
+            try
+            {
+                await sslStream.AuthenticateAsServerAsync(new SslServerAuthenticationOptions
+                {
+                    ServerCertificate = _certificate,
+                    ClientCertificateRequired = false,
+                    CertificateRevocationCheckMode = X509RevocationMode.NoCheck
+                }, cancellationToken);
+
+                DisplaySecurityLevel(sslStream);
+                DisplaySecurityServices(sslStream);
+                DisplayCertificateInformation(sslStream);
+                DisplayStreamProperties(sslStream);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                }
+                Console.WriteLine ("Authentication failed - closing the connection.");
+            }
+            finally
+            {
+                sslStream.Close();
+                client.Close();
+                Console.WriteLine();
+            }
+        }
+
+        private static void DisplaySecurityLevel(SslStream stream)
+        {
+            Console.WriteLine("Cipher: {0} strength {1}", stream.CipherAlgorithm, stream.CipherStrength);
+            Console.WriteLine("Hash: {0} strength {1}", stream.HashAlgorithm, stream.HashStrength);
+            Console.WriteLine("Key exchange: {0} strength {1}", stream.KeyExchangeAlgorithm,
+                stream.KeyExchangeStrength);
+            Console.WriteLine("Protocol: {0}", stream.SslProtocol);
+        }
+
+        private static void DisplaySecurityServices(AuthenticatedStream stream)
+        {
+            Console.WriteLine("Is authenticated: {0} as server? {1}", stream.IsAuthenticated, stream.IsServer);
+            Console.WriteLine("IsSigned: {0}", stream.IsSigned);
+            Console.WriteLine("Is Encrypted: {0}", stream.IsEncrypted);
+        }
+
+        private static void DisplayStreamProperties(Stream stream)
+        {
+            Console.WriteLine("Can read: {0}, write {1}", stream.CanRead, stream.CanWrite);
+            Console.WriteLine("Can timeout: {0}", stream.CanTimeout);
+        }
+
+        private static void DisplayCertificateInformation(SslStream stream)
+        {
+            Console.WriteLine("Certificate revocation list checked: {0}", stream.CheckCertRevocationStatus);
+
+            var localCertificate = stream.LocalCertificate;
+            if (stream.LocalCertificate != null)
+            {
+                Console.WriteLine("Local cert was issued to {0} and is valid from {1} until {2}.",
+                    localCertificate.Subject,
+                    localCertificate.GetEffectiveDateString(),
+                    localCertificate.GetExpirationDateString());
+            }
+            else
+            {
+                Console.WriteLine("Local certificate is null.");
+            }
+
+            // Display the properties of the client's certificate.
+            var remoteCertificate = stream.RemoteCertificate;
+            if (stream.RemoteCertificate != null)
+            {
+                Console.WriteLine("Remote cert was issued to {0} and is valid from {1} until {2}.",
+                    remoteCertificate?.Subject,
+                    remoteCertificate?.GetEffectiveDateString(),
+                    remoteCertificate?.GetExpirationDateString());
+            }
+            else
+            {
+                Console.WriteLine("Remote certificate is null.");
+            }
+        }
+    }
+}
