aa58ae35b5dc1be29f1a3df84c89897e2ca2b1c0
aiden <aidenwelsh@outlook.com>
2020-11-24 16:40:45 +0000

    better logging part 1

--- TLS.Client/Program.cs
+++ TLS.Client/Program.cs
@@ -9,6 +9,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using McMaster.Extensions.CommandLineUtils;
 using Newtonsoft.Json;
+using Serilog;
+using Serilog.Events;
 
 namespace TLS.Client
 {
@@ -22,75 +24,90 @@ namespace TLS.Client
         [Option("-p|--port", Description = "The port to connect on. Defaults to 443.")]
         private int Port { get; } = 443;
 
-        [Option("-v|--verbose")] private bool Verbose { get; } = false;
-
-        private static async Task<int> Main(string[] args) =>
+        [Option("-l|--logEventLevel", Description = "The verbosity of the output from the app processing. Defaults to [Information]")] 
+        private LogEventLevel LogEventLevel { get; } = LogEventLevel.Information;
+        
+        private static async Task<int> Main(string[] args) => 
             await CommandLineApplication.ExecuteAsync<Program>(args);
 
         private async Task<int> OnExecuteAsync(CommandLineApplication app,
             CancellationToken cancellationToken = default)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Is(LogEventLevel)
+                //.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting up...\n");
+
+                await CreateTlsClientAsync(app, cancellationToken);
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Host terminated unexpectedly");
+                return 0;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+            return 1;
+        }
+
+        private async Task CreateTlsClientAsync(CommandLineApplication app,
+            CancellationToken cancellationToken = default)
+        {
             if (string.IsNullOrWhiteSpace(TargetEndpoint))
             {
                 app.ShowHelp();
-                return 0;
             }
-
-            if (Verbose)
-            {
-                Console.WriteLine("Starting TLS.Client app...\n");
-            }
-
+            
             var status = new ProtocolStatus
             {
                 TargetEndpoint = TargetEndpoint,
                 Port = Port
             };
 
-            if (Verbose)
-                Console.WriteLine("Testing against list of known protocols.\n");
+            Log.Information("Created an empty result set ready for processing: {@status}", status);
+
+            Log.Information("Testing against list of known protocols.\n");
 
             foreach (SslProtocols protocol in Enum.GetValues(typeof(SslProtocols)))
             {
-                if (Verbose)
-                {
-                    Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-                    Console.WriteLine($"Begin building test for protocol [{protocol}]");
-                }
-
+                Log.Verbose("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                Log.Verbose("Begin building test for protocol [{@protocol}]", protocol);
+                
                 var client = new TcpClient();
 
                 try
                 {
-                    if (Verbose)
-                        Console.WriteLine($"Attempting to connect to [{TargetEndpoint}:{Port}]");
+                    Log.Verbose($"Attempting to connect to [{TargetEndpoint}:{Port}]");
 
                     await client.ConnectAsync(TargetEndpoint, Port);
                     status.ConnectionSuccessful = true;
-                    if (Verbose)
-                        Console.WriteLine(
-                            $"Connection [{(status.ConnectionSuccessful ? "Successful" : "Unsuccessful")}]");
+                    Log.Verbose("Connection [{@result}]", 
+                        status.ConnectionSuccessful ? "Successful" : "Unsuccessful");
                 }
                 catch
                 {
                     status.ConnectionSuccessful = false;
 
-                    if (Verbose)
-                        Console.WriteLine(
-                            $"Connection [{(status.ConnectionSuccessful ? "Successful" : "Unsuccessful")}]");
-
-                    return 0;
+                    Log.Verbose("Connection [{@result}]", 
+                        status.ConnectionSuccessful ? "Successful" : "Unsuccessful");
                 }
 
-                if (Verbose)
-                    Console.WriteLine("Creating SSL/TLS connection stream");
+                Log.Verbose("Creating SSL/TLS connection stream");
 
                 var sslStream = new SslStream(client.GetStream(), false);
 
                 try
                 {
-                    if (Verbose)
-                        Console.WriteLine($"Attempting to authenticate as a client via protocol [{protocol}]");
+                    Log.Verbose($"Attempting to authenticate as a client via protocol [{protocol}]");
 
                     await sslStream.AuthenticateAsClientAsync(new SslClientAuthenticationOptions
                     {
@@ -99,16 +116,11 @@ namespace TLS.Client
                         CertificateRevocationCheckMode = X509RevocationMode.NoCheck,
                         RemoteCertificateValidationCallback = RemoteCertificateValidationCallback
                     }, cancellationToken);
-
-                    //sslStream.AuthenticateAsClient(Endpoint, null, protocol, false);
-
-                    if (Verbose)
-                    {
-                        Console.WriteLine(
-                            $"Authentication [{(sslStream.IsAuthenticated ? "Successful" : "Unsuccessful")}]");
-                        Console.WriteLine("Gathering certificate details");
-                    }
-
+                    
+                    Log.Verbose("Authentication [{@result}]", 
+                        sslStream.IsAuthenticated ? "Successful" : "Unsuccessful");
+                    Log.Verbose("Gathering certificate details");
+                    
                     status.Certificate = new Certificate
                     {
                         Subject = sslStream.RemoteCertificate?.Subject,
@@ -117,41 +129,33 @@ namespace TLS.Client
                         NotAfter = sslStream.RemoteCertificate?.GetExpirationDateString(),
                     };
 
-                    if (Verbose)
-                        Console.WriteLine($"Adding successful attempt for protocol [{protocol}]");
+                    Log.Verbose($"Adding successful attempt for protocol [{protocol}]");
 
                     status.Protocols.Add(protocol, true);
                 }
-                catch (AuthenticationException e)
+                catch (Exception e)
                 {
-                    if (Verbose)
+                    Log.Verbose("Exception: {0}", e.Message);
+                    if (e.InnerException != null)
                     {
-                        Console.WriteLine("Exception: {0}", e.Message);
-                        if (e.InnerException != null)
-                        {
-                            Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
-                        }
-                        Console.WriteLine ("Authentication failed - closing the connection.");
-                        Console.WriteLine($"Adding unsuccessful attempt for protocol [{protocol}]");
+                        Log.Verbose("Inner exception: {0}", e.InnerException.Message);
                     }
-
+                    Log.Verbose("Authentication failed - closing the connection.");
+                    Log.Verbose("Adding unsuccessful attempt for protocol [{@protocol}]", protocol);
+                    
                     status.Protocols.Add(protocol, false);
                 }
                 finally
                 {
-                    if (Verbose)
-                        Console.WriteLine("Closing the SSL/TLS connection stream");
+                    Log.Verbose("Closing the SSL/TLS connection stream");
 
                     sslStream.Close();
 
-                    if (Verbose)
-                        Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+                    Log.Verbose("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
                 }
             }
 
             Console.WriteLine(JsonConvert.SerializeObject(status, Formatting.Indented));
-
-            return 1;
         }
 
         private static bool RemoteCertificateValidationCallback(object sender, X509Certificate certificate,
